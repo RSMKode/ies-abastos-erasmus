d5953cf77edf11838eb8a49d4362bf7b13c935f1
Roger Sancho <rogersanchom@gmail.com>
2024-04-01 19:55:47 +0200

    add netlify hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rsmkode-blog.netlify.app",
-  integrations: [preact()]
+  site: "https://ies-erasmus.netlify.app",
+  integrations: [preact()],
+  output: "hybrid",
+  adapter: netlify(),
 });
