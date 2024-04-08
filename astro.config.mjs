c988c435b6fe87b6d69e2a809eca8a434ffc2d30
Roger Sancho <rogersanchom@gmail.com>
2024-04-08 18:11:01 +0200

    Actualizar configuración de Astro para incluir integraciones de icono y tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import netlify from "@astrojs/netlify";
+import icon from "astro-icon";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ies-erasmus.netlify.app",
-  integrations: [preact()],
+  integrations: [preact(), icon(), tailwind()]
   // output: "hybrid",
   // adapter: netlify(),
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+import animations from "@midudev/tailwind-animations";
+
+import colors from "tailwindcss/colors";
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        // primary: "rgb(var(--color-primary) / <alpha-value>)",
+        primary: "rgb(250 204 21 / <alpha-value>)",
+        // secondary: "rgb(0 0 0 / <alpha-value>)",
+        secondary: colors.white,
+        accent: {
+          // 1: "rgb(var(--color-accent-1) / <alpha-value>)",
+          // 1: "rgb(250 204 21 / <alpha-value>)",
+          1: colors.yellow[300],
+          2: colors.rose[900],
+        },
+        // bkg: "hsl(var(--color-bkg) / <alpha-value>)",
+        // content: "hsl(var(--color-content) / <alpha-value>)",
+      },
+      backgroundImage: "var(--bkg-gradient)",
+      animation: {
+        "spin-faster": "spin 0.5s ease infinite",
+        "spin-fast": "spin 0.75s ease infinite",
+        "spin-slower": "spin 35s ease infinite",
+        "spin-slow": "spin 25s ease-in-out infinite reverse",
+      },
+    },
+  },
+  plugins: [animations],
+};
