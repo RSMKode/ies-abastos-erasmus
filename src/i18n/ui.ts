e7ab0458f5eba4724940f0dda69709eda378582c
Roger <rogersanchom@gmail.com>
2024-05-13 11:22:10 +0200

    Eliminar comentario de posible traduccion de rutas

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -19,22 +19,3 @@ export const defaultLang = 'es';
 export const getLocaleTranslation = (currentLocale: string | undefined) => {
   return currentLocale && currentLocale === 'es' ? es : en;
 };
-
-// export const routes = {
-//   es: {
-//     vota: 'vota',
-//     info: 'info',
-//     archivo: 'archivo',
-//     'aviso-legal': 'aviso-legal',
-//     privacidad: 'privacidad',
-//     cookies: 'cookies',
-//   },
-//   en: {
-//     vota: 'vote',
-//     info: 'information',
-//     archivo: 'archive',
-//     'aviso-legal': 'legal-notice',
-//     privacidad: 'privacy',
-//     cookies: 'cookies',
-//   },
-// } as const;
